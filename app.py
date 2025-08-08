b8bbaa1571c859514e8ae0cd1f1808cdec859722
Prajwal Malokar <malokarprajwal4@gmail.com>
2025-08-08 22:01:14 +0530

    Added Home Page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
